19d982c1b517a1bab0df74affae84396fe09640f
sam06tanb <samirtajra53@gmail.com>
2023-11-12 14:11:10 -0300

    Initial commit

---
+++ src/entities/HourContract.java
@@ -0,0 +1,56 @@
+package entities;
+import java.util.Date;
+
+public class HourContract {
+
+    private Date date;
+    private Double value;
+    private Integer hours;
+
+    private Double valuePerHour;
+
+    public HourContract(){
+    }
+
+    public HourContract(Date date, Double valuePerHour, Integer hours) {
+        this.date = date;
+        this.valuePerHour = valuePerHour;
+        this.hours = hours;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Double getValuePerHour() {
+        return valuePerHour;
+    }
+
+    public void setValuePerHour(Double valuePerHour) {
+        this.valuePerHour = valuePerHour;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+
+    public double totalValue() {
+        return  valuePerHour * hours;
+    }
+}
